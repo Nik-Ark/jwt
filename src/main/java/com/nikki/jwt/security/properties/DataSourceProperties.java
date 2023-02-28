c58d6fd6a2bffbf96aa0b3d31c7e7e674150ee3b
Nikita Alekseev <lksndrmkdnsky@gmail.com>
2023-03-01 02:04:31 +0500

    host and port variables added to app.properties

--- src/main/java/com/nikki/jwt/security/properties/DataSourceProperties.java
+++ src/main/java/com/nikki/jwt/security/properties/DataSourceProperties.java
@@ -4,5 +4,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(value = "db")
 public record DataSourceProperties(
-        String url, String name, boolean allowPKRetrieval, boolean useSSL, String username, String password
+        String url, int port, String name, boolean allowPKRetrieval, boolean useSSL, String username, String password
 ) {}
