3909d320ea7be82b0ada9a8e608508eeb4cd7167
Nikita Alekseev <lksndrmkdnsky@gmail.com>
2023-03-29 20:08:13 +0500

    Fixed Register and Login Request Dtos (added Getters)

--- src/main/java/com/nikki/jwt/security/dto/LoginResponseDto.java
+++ src/main/java/com/nikki/jwt/security/dto/LoginResponseDto.java
@@ -1,7 +1,9 @@
 package com.nikki.jwt.security.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 public class LoginResponseDto {
 
--- src/main/java/com/nikki/jwt/security/dto/RegisterResponseDto.java
+++ src/main/java/com/nikki/jwt/security/dto/RegisterResponseDto.java
@@ -1,7 +1,11 @@
 package com.nikki.jwt.security.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
+import java.util.Arrays;
+
+@Getter
 @Builder
 public class RegisterResponseDto {
 
@@ -11,4 +15,16 @@ public class RegisterResponseDto {
     private String[] roles;
     private String accessToken;
     private String refreshToken;
+
+    @Override
+    public String toString() {
+        return "RegisterResponseDto{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", roles=" + Arrays.toString(roles) +
+                ", accessToken='" + accessToken + '\'' +
+                ", refreshToken='" + refreshToken + '\'' +
+                '}';
+    }
 }
