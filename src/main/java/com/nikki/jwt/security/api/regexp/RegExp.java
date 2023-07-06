3744170bdbc3658dbd4dc6d92bffeb2c1587e076
Nikita Alekseev <lksndrmkdnsky@gmail.com>
2023-07-06 17:02:57 +0500

    Before refactoring Register Component (on Frontend)

--- src/main/java/com/nikki/jwt/security/api/regexp/RegExp.java
+++ src/main/java/com/nikki/jwt/security/api/regexp/RegExp.java
@@ -4,5 +4,5 @@ public class RegExp {
     public final static String name = "^[A-zА-яЁё]{1,44}$";
     public final static String email = "^[A-z0-9._%+-]+@[A-z0-9.-]+\\.[A-z]{1,44}$";
     public final static String password =
-            "(?=[\\x21-\\x7E]*?[0-9])(?=[\\x21-\\x7E]*?[a-z])(?=[\\x21-\\x7E]*?[A-Z])(?=[\\x21-\\x7E]*?[!@#$%^&+=])[\\x21-\\x7E]{8,44}";
+"(?=[\\x21-\\x7E]*?[0-9])(?=[\\x21-\\x7E]*?[a-z])(?=[\\x21-\\x7E]*?[A-Z])(?=[\\x21-\\x7E]*?[!@#$%^&+=])[\\x21-\\x7E]{8,44}";
 }
